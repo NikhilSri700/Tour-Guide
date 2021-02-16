50831a5a887c43d3f88f9c742c57c22904875094
Nikhil <nikhilsrivastav700@gmail.com>
2021-02-16 17:36:21 +0530

    Added Splash screen

--- app/src/main/java/com/example/tourguide/MainActivity.java
+++ app/src/main/java/com/example/tourguide/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.tourguide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Animation topAnim, bottomAnim;
+    ImageView appLogo, appName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        appLogo = findViewById(R.id.splash_logo);
+        appName = findViewById(R.id.splash_name);
+
+        appLogo.setAnimation(topAnim);
+        appName.setAnimation(bottomAnim);
+
     }
 }
